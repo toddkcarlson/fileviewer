2777fe4d3348f2f5d7fd888184e47ccf4f868999
Todd Carlson <tcarlson@carlsonsw.com>
2018-08-01 10:22:56 -0400

    production enivorment fix: attempt2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
-  config.read_encrypted_secrets = true  
+  #config.read_encrypted_secrets = true  
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
