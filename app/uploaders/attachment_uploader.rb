24aa4b08bf35c771c091d86845e3d23c6bb0913c
Todd Carlson <tcarlson@carlsonsw.com>
2018-08-28 08:54:45 -0400

    session to url

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -9,13 +9,14 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
+    @current_session = session[:session_id]    
+    "uploads/#{model.current_session}/"
  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
  #   "uploads/temp/"
-    "uploads/#{model.current_session}/"
  #    "uploads/#{model.send(:current_session)}"
   end
 
